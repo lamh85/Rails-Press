587f546b92351008f6a6306b3a029b7bb81cefb1
Hon Lam <lamh85@gmail.com>
2015-09-27 15:50:07 -0700

    Post model: added seeding method.

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,17 @@ class Post < ActiveRecord::Base
 
     has_many :comments
     belongs_to :user
+    attr_accessor :title
+    attr_accessor :slug
 
+    def self.seed(number)
+        number.times do |x|
+          post = Post.new
+          title = Faker::Lorem.paragraph(1)
+          post.title = Faker::Lorem.paragraph(1)
+          post.slug = post.title.parameterize.first(50)
+          post.content = Faker::Lorem.paragraph(4)
+          post.save
+        end
+    end
 end
