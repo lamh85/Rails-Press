a0aa1e94ca099a55a855a61cacff9a7b4ace2c55
Hon Lam <lamh85@gmail.com>
2015-11-27 23:12:48 -0800

    Posts Ctrl: Added skeleton actions.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,28 @@
 class PostsController < ApplicationController
 
+    # TABLE OF CONTENTS:
+    # Create, Read, Update, Destroy, Private
+
+    def new
+    end
+
     def create
         post = Post.new
         if post.slug == false || post.slug == ""
             post.slug = post.title.parameterize.first(50)
         end
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+    private
+
+    def post_params
+        params.require(:post).permit()
     end
 
 end
