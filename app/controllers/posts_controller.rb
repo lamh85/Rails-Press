0c83b80a8488f96f5570c8173d165cc364d26cc5
Hon Lam <lamh85@gmail.com>
2015-11-28 13:55:54 -0800

    Refactored Post controller.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,11 +4,12 @@ class PostsController < ApplicationController
     # Create, Read, Update, Destroy, Private
 
     def new
+        @post = Post.new
     end
 
     def create
-        post = Post.new
-        if post.slug == false || post.slug == ""
+        post = Post.new(post_params)
+        if post.slug.strip == (false || "" || nil)
             post.slug = post.title.parameterize.first(50)
         end
     end
