72c9f33db054c6e697955b8360f6337192d72231
Hon Lam <lamh85@gmail.com>
2015-12-04 22:12:49 -0800

    Created Tag model.

--- app/assets/javascripts/post_new.js
+++ app/assets/javascripts/post_new.js
@@ -3,4 +3,13 @@ var postNewApp = angular.module("postNewApp",[]);
 formController = postNewApp.controller("formController", ['$scope', '$http', function($scope, $http){
     console.log('testing')
 
+    $scope.inputFields = [
+        {}
+
+    // label
+    // input name
+    // input type
+        
+    ]
+
 }]);
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151007031207) do
+ActiveRecord::Schema.define(version: 20151205061019) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,14 @@ ActiveRecord::Schema.define(version: 20151007031207) do
   end
 
   add_index "posts", ["user_id"], name: "index_posts_on_user_id", using: :btree
+
+  create_table "tags", force: :cascade do |t|
+    t.string   "name"
+    t.string   "slug"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
