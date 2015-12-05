1e5586e142e41c3ca505993603b9ac237c505dbd
Hon Lam <lamh85@gmail.com>
2015-12-05 15:29:52 -0800

    Added avatar column to users.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151205065422) do
+ActiveRecord::Schema.define(version: 20151205232853) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 20151205065422) do
     t.string   "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "avatar"
   end
 
   add_foreign_key "pages", "users"
