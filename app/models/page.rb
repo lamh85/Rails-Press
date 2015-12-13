349aafb7a5178e6f18e7b00e50661018522618ec
Hon Lam <lamh85@gmail.com>
2015-12-12 23:44:02 -0800

    Started validation rules for Page.

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,4 +1,13 @@
 class Page < ActiveRecord::Base
+
+    # Validations
+    # -----------
+
+    validates :heading, presence: {message: "The webpage heading cannot be blank."}
+
+    # Relations
+    # ---------
+
     has_one :post
     has_one :tag
     has_many :code_files
