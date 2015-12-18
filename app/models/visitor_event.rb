21c1e0ea51d7d1e7d53278ecf1ac96f9160ff2b7
Hon Lam <lamh85@gmail.com>
2015-12-17 18:11:20 -0800

    Renam model to Visit.

--- app/models/visitor_event.rb
+++
@@ -1,2 +0,0 @@
-class VisitorEvent < ActiveRecord::Base
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151216085250) do
+ActiveRecord::Schema.define(version: 20151217011655) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -86,7 +86,7 @@ ActiveRecord::Schema.define(version: 20151216085250) do
     t.datetime "updated_at",      null: false
   end
 
-  create_table "visitor_events", force: :cascade do |t|
+  create_table "visits", force: :cascade do |t|
     t.string   "ip_address"
     t.string   "event"
     t.datetime "time"
