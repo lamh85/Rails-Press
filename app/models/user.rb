65fcf45c1bae22a1a6cbbc30e298c669c0fa82ab
Hon Lam <lamh85@gmail.com>
2015-12-20 17:20:17 -0800

    Added validations for User.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,14 @@
 class User < ActiveRecord::Base
 
+    # Validations
+    # -----------
+
+    validates :password, length: {in: 6..20, message: "Your password must contain between 6 and 20 characters."}
+    validates :email, format: { with: /[^@]*@[^@]*\.[^@]{2,4}/, message: "Your email is invalid." }
+
+    # Relations
+    # ---------
+
     has_many :posts
     has_many :comments
     
