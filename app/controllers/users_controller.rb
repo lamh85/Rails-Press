a7c0a232b48bf35072808a1ca78619e2855e67a0
Hon Lam <lamh85@gmail.com>
2015-12-20 18:33:16 -0800

    Worked on UsersController#create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,19 @@ class UsersController < ApplicationController
     end
 
     def create
+        user = User.new(user_params)
+        if user.save
+            session[:user_id] = user.id
+            render text: 'success!'
+        else
+            redirect_to '/register'
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(*User.attrs_array)
     end
 
 end
