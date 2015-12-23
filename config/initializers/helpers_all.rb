c7537e032efd8654cfbd23c1b080bc59977eb65b
Hon Lam <lamh85@gmail.com>
2015-12-22 19:53:52 -0800

    Created helper that returns all DB table names.

--- config/initializers/helpers_all.rb
+++ config/initializers/helpers_all.rb
@@ -3,4 +3,8 @@ class Object
         return true if self == nil || self.strip.length == 0
         return false
     end
+
+    def db_tables
+        ActiveRecord::Base.connection.tables.map { |element| element.to_s }
+    end
 end
