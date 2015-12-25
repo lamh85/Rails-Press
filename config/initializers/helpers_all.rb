059f7ccc295276ed2b5c64eb068192a68ed9c9ad
Hon Lam <lamh85@gmail.com>
2015-12-24 17:12:06 -0800

    Refactored indentation.

--- config/initializers/helpers_all.rb
+++ config/initializers/helpers_all.rb
@@ -5,8 +5,8 @@ class Object
     end
 
     def invalid_slugs
-        db_tables = ActiveRecord::Base.connection.tables.map { |element| element.to_s }
+        db_tables   = ActiveRecord::Base.connection.tables.map { |element| element.to_s }
         controllers = Dir[Rails.root.join('app/controllers/*_controller.rb')].map { |path| (path.match(/(\w+)_controller.rb/); $1) }
-        db_tables + controllers
+        (db_tables + controllers).uniq
     end
 end
